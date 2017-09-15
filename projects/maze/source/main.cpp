03926922cfe3886cec91ec86e1fbad4becd24195
Zhao Tianyu <davidxk@qq.com>
2017-09-14 17:13:03 -0700

    Make maze randomized

--- projects/maze/source/main.cpp
+++ projects/maze/source/main.cpp
@@ -1,9 +1,12 @@
 #include "Magician.h"
 
 #include "HelloWorld.h"
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
+	srand(time(NULL));
 	Scene* hello = new HelloWorld();
 	gDirector->runWithScene( hello );
 	return 0;
